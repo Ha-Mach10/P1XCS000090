df29237d6413d288695c7c40c833c315a016e1a6
知らん仮名 <momiji0846to1026@outlook.com>
2024-11-08 07:47:55 +0900

    クラスの更新。 拡大実行のメソッドを実装中。

--- P1XCS000090/Shapes/DrLine.cs
+++ P1XCS000090/Shapes/DrLine.cs
@@ -8,7 +8,7 @@ using System.Windows.Media;
 
 namespace P1XCS000090.Shapes
 {
-	public class DrLine
+	public class DrLine : DrPoint
 	{
 		// *******************************************************************************
 		// Static Readonly Fields
@@ -31,9 +31,9 @@ namespace P1XCS000090.Shapes
 		// *******************************************************************************
 
 		public int Id { get; }
-		public Point First { get; }
-		public Point Second { get; }
-		public Point CenterPoint { get; }
+		public DrPoint First { get; }
+		public DrPoint Second { get; }
+		public DrPoint CenterPoint { get; }
 		public Pen Pen { get; }
 
 
@@ -42,18 +42,18 @@ namespace P1XCS000090.Shapes
 		// Constructors
 		// *******************************************************************************
 
-		public DrLine(Point first, Point second, Pen pen)
+		public DrLine(Point first, Point second, Pen pen) : base()
 		{
 			_idCount++;
 
 			Id = _idCount;
-			First = first;
-			Second = second;
+			First = new DrPoint(first);
+			Second = new DrPoint(second);
 			Pen = pen;
 
 			double x = First.X <= Second.X ? First.X - Second.X : Second.X - First.X;
 			double y = First.Y >= Second.Y ? First.Y - Second.Y : Second.Y - First.Y;
-			CenterPoint = new Point(x, y);
+			CenterPoint = new DrPoint(x, y);
 		}
 		public DrLine(int id, Point first, Point second, Pen pen)
 			: this(first, second, pen)
@@ -73,7 +73,7 @@ namespace P1XCS000090.Shapes
 		/// <param name="dc"></param>
 		public void DraftLine(DrawingContext dc)
         {
-			dc.DrawLine(Pen, First, Second);
+			dc.DrawLine(Pen, DrPointToPoint(First), DrPointToPoint(Second));
         }
 		public void DraftLine(DrawingContext dc, Point first, Point second, double ratio, Point cursorPosition)
         {
@@ -81,6 +81,22 @@ namespace P1XCS000090.Shapes
 			Point s = new Point(second.X * ratio, second.Y * ratio);
 			dc.DrawLine(Pen, f, s);
         }
+		public void ReScaleLine(double rate, Point cursorPoint, DrLine targetLine)
+		{
+			DrPoint drPointFirst = base.ReScale(rate, cursorPoint, targetLine.First);
+			DrPoint drPointSecond = base.ReScale(rate, cursorPoint, targetLine.Second);
+
+			
+		}
+
+
+
+		// *******************************************************************************
+		// Public Methods
+		// *******************************************************************************
+
+		private Point DrPointToPoint(DrPoint point)
+			=> new Point(point.X, point.Y);
 
 
 
--- P1XCS000090/Shapes/DrPoint.cs
+++ P1XCS000090/Shapes/DrPoint.cs
@@ -15,48 +15,65 @@ namespace P1XCS000090.Shapes
 		public double Y { get; }
 
 
-
-		public DrPoint(double x, double y)
+		public DrPoint()
+		{
+			X = 0;
+			Y = 0;
+		}
+		public DrPoint(double x, double y) : this()
 		{
 			X = x;
 			Y = y;
+		}
+		public DrPoint(Point point) : this()
+		{
+			X = point.X;
+			Y = point.Y;
 		}
 
 
 
 
 		public DrPoint ReScale(double rate, Point cursorPosition)
+			=> ReScale(rate, cursorPosition, this);
+
+		public DrPoint ReScale(double rate, Point cursorPosition, DrPoint targetPoint)
 		{
+			// カーソル点を仮想原点とし、仮想原点からのＸの位置を定義
+			double ImaginalyOriginX = targetPoint.X - cursorPosition.X;
+			// 仮想原点から位置Ｘへ倍率を乗算
+			double reScaledPositionX = ImaginalyOriginX * rate;
+
+			// 
 			double reScaleX = 0;
-			if (cursorPosition.X > X)
+			if (cursorPosition.X > targetPoint.X)
 			{
-				// カーソル点を仮想原点とし、仮想原点からのＸの位置を定義
-				double ImaginalyOriginX = X - cursorPosition.X;
-				// 仮想原点から位置Ｘへ倍率を乗算
-				double reScaledPositionX = ImaginalyOriginX * rate;
 				// 仮想原点としたカーソル点を加算し、実際の位置Ｘを算出
 				reScaleX = cursorPosition.X + reScaledPositionX;
 			}
 			else
 			{
-				// カーソル点を仮想原点とし、仮想原点からのＸの位置を定義
-				double ImaginalyOriginX = cursorPosition.X + (cursorPosition.X - X);
-				// 仮想原点から位置Ｘへ倍率を乗算
-				double reScaledPositionX = ImaginalyOriginX * rate;
 				// 仮想原点としたカーソル点を加算し、実際の位置Ｘを算出
-				reScaleX = cursorPosition.X + reScaledPositionX;
+				reScaleX = cursorPosition.X - reScaledPositionX;
 			}
 
+
+			// カーソル点を仮想原点とし、仮想原点からのＹの位置を定義
+			double imaginaryOriginY = targetPoint.Y - cursorPosition.Y;
+			// 仮想原点から位置Ｙへ倍率を乗算
+			double reScaledPositionY = imaginaryOriginY * rate;
+
+			// 
 			double reScaleY = 0;
-			if (cursorPosition.Y > Y)
+			if (cursorPosition.Y > targetPoint.Y)
 			{
-				double imaginaryOriginY = Y - cursorPosition.Y;
-				double reScaledPositionY = imaginaryOriginY * rate;
+				// 仮想原点としたカーソル点を加算し、実際の位置Ｙを算出
 				reScaleY = cursorPosition.Y + reScaledPositionY;
 			}
 			else
 			{
-
+				// 仮想原点としたカーソル点を加算し、実際の位置Ｙを算出
+				reScaleY = cursorPosition.Y - reScaledPositionY;
 			}
 
 			return new DrPoint(reScaleX, reScaleY);
